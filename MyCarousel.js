980f4e35084cff32fc6b8c9e9f7e33f1fa835e65
berkcansmet <berkcansametgurdal@gmail.com>
2020-01-06 18:16:55 +0300

    06.01.2020

--- MyCarousel.js
+++ MyCarousel.js
@@ -1,21 +1,27 @@
 //import liraries
+import { Header, Right, Left, Body } from "native-base";
 import React, { Component } from "react";
 import {
   View,
   Text,
   StyleSheet,
-  ImageBackground,
   SafeAreaView,
   Dimensions,
   TouchableOpacity,
+  TouchableHighlight,
+  Modal,
 } from "react-native";
-import { Header, Right, Left, Body } from "native-base";
+import ImageViewer from "react-native-image-zoom-viewer";
 import { Actions } from "react-native-router-flux";
-import Carousel, { Pagination, ParallaxImage } from "react-native-snap-carousel";
+import Carousel, {
+  Pagination,
+  ParallaxImage,
+} from "react-native-snap-carousel";
 import Icon from "react-native-vector-icons/FontAwesome";
 import Dizi from "./Dizi";
+
 // create a component
-const { width: screenWidth } = Dimensions.get('window')
+const { width: screenWidth } = Dimensions.get("window");
 class MyCarousel extends Component {
   constructor(props) {
     super(props);
@@ -23,47 +29,52 @@ class MyCarousel extends Component {
       carouselItems: [
         {
           title: "Berkcan",
-          image:
-            "https://images.unsplash.com/photo-1502320332898-8ec6e74e147f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+          url: "https://s.cdpn.io/profiles/user/30606/512.jpg?2",
         },
         {
           title: "Berkcan 2",
-          image:
-            "https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+          url: "https://avatars1.githubusercontent.com/u/2545400?s=460&v=4",
         },
         {
           title: "Berkcan 3",
-          image:
-            "https://images.unsplash.com/photo-1512389055488-8d82cb26ba6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+          url:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjHzmqdgCjZlzoeKv-fzDnc8-qL5nzzsJVYRZ4Pf8861exdthgrw&s",
         },
         {
           title: "Berkcan 4",
-          image:"https://images.unsplash.com/photo-1509114397022-ed747cca3f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+          url:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8Eqcv6VifKYhFsfsA2f7xISqGizybqiL410U-g18aPzP6sHkm&s",
         },
         {
           title: "Berkcan 5",
-          image:
+          url:
             "https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
         },
       ],
+      modalVisible: false,
     };
   }
+  handleChange = index => {
+    this.setState({ currentIndex: index });
+  };
   renderItem({ item, index }, parallaxProps) {
+    console.log("index", index);
     return (
-<View style={styles.item}>
-                <ParallaxImage
-                    source={{ uri: item.image }}
-                    containerStyle={styles.imageContainer}
-                    style={styles.image}
-                    parallaxFactor={1}
-                    {...parallaxProps}
-                />
-                <Text style={styles.Te} numberOfLines={2}>
-                    { item.title }
-                </Text>
-            </View>
+      <View style={styles.item}>
+        <ParallaxImage
+          source={{ uri: item.url }}
+          containerStyle={styles.imageContainer}
+          style={styles.image}
+          parallaxFactor={1}
+          {...parallaxProps}
+        />
+        <Text style={styles.Te} numberOfLines={2}>
+          {item.title}
+        </Text>
+      </View>
     );
   }
+
   get pagination() {
     const { carouselItems, activeSlide } = this.state;
     return (
@@ -83,33 +94,62 @@ class MyCarousel extends Component {
       />
     );
   }
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
-      <Header style={styles.Header}>
-        <Left/>
-        <Body>
-          <Text style={styles.text} numberOfLines={1}>
-            {this.props.title}
-          </Text>
-        </Body>
-        <Right>
-        <TouchableOpacity onPress={() => Actions.Dizi()}>
-            <Icon name="chevron-right" size={30} color="black" />
-          </TouchableOpacity></Right>
-      </Header>
-        <Carousel
-          ref={c => {
-            this._carousel = c;
+        <View>
+          <Modal
+            visible={this.state.modalVisible}
+            transparent={true}
+            swipeDirection={"down"}
+            onRequestClose={() => {
+              this.setState({ modalVisible: false });
+            }}
+          >
+            <ImageViewer
+              imageUrls={this.state.carouselItems}
+              enableSwipeDown={true}
+              onCancel={() => {
+                this.setState({ modalVisible: false });
+              }}
+            />
+          </Modal>
+        </View>
+        <Header style={styles.Header}>
+          <Left />
+          <Body>
+            <Text style={styles.text} numberOfLines={1}>
+              {this.props.title}
+            </Text>
+          </Body>
+          <Right>
+            <TouchableOpacity onPress={() => Actions.Dizi()}>
+              <Icon name="chevron-right" size={30} color="black" />
+            </TouchableOpacity>
+          </Right>
+        </Header>
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalVisible(true);
           }}
-          data={this.state.carouselItems}
-          renderItem={this.renderItem}
-          sliderWidth={screenWidth}
-          sliderHeight={screenWidth}
-          itemWidth={screenWidth - 60}
-          onSnapToItem={index => this.setState({ activeSlide: index })}
-          hasParallaxImages={true}
-        />
+        >
+          <Carousel
+            ref={c => {
+              this._carousel = c;
+            }}
+            data={this.state.carouselItems}
+            renderItem={this.renderItem}
+            sliderWidth={screenWidth}
+            sliderHeight={screenWidth}
+            itemWidth={screenWidth - 60}
+            onSnapToItem={index => this.setState({ activeSlide: index })}
+            hasParallaxImages={true}
+          />
+        </TouchableHighlight>
         {this.pagination}
       </SafeAreaView>
     );
@@ -132,26 +172,29 @@ const styles = StyleSheet.create({
   },
   Header: {
     width: "100%",
-    backgroundColor:"white"
+    backgroundColor: "white",
   },
   icon: {
     alignItems: "flex-end",
-    margin:10,
+    margin: 10,
   },
-   item: {
+  item: {
     width: screenWidth - 60,
     height: screenWidth - 60,
-    marginTop:screenWidth -250,
+    marginTop: screenWidth - 250,
   },
   imageContainer: {
     flex: 1,
     marginBottom: Platform.select({ ios: 0, android: 1 }),
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 8,
   },
   image: {
-
-    resizeMode: 'cover',
+    resizeMode: "cover",
+  },
+  MainContainer: {
+    flex: 1,
+    alignItems: "center",
   },
 });
 
